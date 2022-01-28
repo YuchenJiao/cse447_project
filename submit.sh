ad7da71f8d61766adb1862b729865139c63b4540
YuchenJiao <jyc0418@uw.edu>
2022-01-27 18:54:24 -0800

    setup environment for submission

--- submit.sh
+++ submit.sh
@@ -6,13 +6,13 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Merun Subhas, jsub\nYuchen Jiao, jyc0418" > submit/team.txt
 
 # train model
-python src/myprogram.py train --work_dir work
+python3 src/CSE447_proj.py train --work_dir work
 
 # make predictions on example data submit it in pred.txt
-python src/myprogram.py test --work_dir work --test_data example/input.txt --test_output submit/pred.txt
+python3 src/CSE447_proj.py test --work_dir work --test_data example/input.txt --test_output submit/pred.txt
 
 # submit docker file
 cp Dockerfile submit/Dockerfile
